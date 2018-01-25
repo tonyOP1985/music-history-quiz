faa3be556cf6e71a0b5edce0aad4ef08f1f97723
tonyOP1985 <toliverpaull@gmail.com>
2018-01-25 17:35:14 -0500

    Heroku deployment prep

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 
+app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')))
+app.use('/public', express.static(path.join(__dirname, 'public')))
+
 app.get('/', (req,res) => {
-  res.send('It works!')
+  res.sendFile(path.join(__dirname + '/index.html'))
 })
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`)
